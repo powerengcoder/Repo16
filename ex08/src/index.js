97609193e1b7589b797635bc4efa5e84ab80e326
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-12 14:17:04 +0200

    Add ex 08

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(name, email, age) {
+    this._name = name;
+    this._email = email;
+    this._age = age;
+  }
+
+  getPerson() {
+    let personInfo = `Name: ${this._name}, email: ${this._email}, age: ${this._email}`;
+    return personInfo;
+  }
+}
+
+module.exports = Person;
